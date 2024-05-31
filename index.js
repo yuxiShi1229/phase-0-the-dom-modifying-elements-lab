f355bd1d32298096a650c68261346e2f801cd128
yuxi Shi <ys3599@tc.columbia.edu>
2024-05-31 09:17:31 -0400

    complete dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Yuxi Shi is the champion';
+document.body.appendChild(newHeader);
